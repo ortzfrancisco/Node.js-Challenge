f87d8463f0dafd1772eff6466b5584139ca53e10
Francisco Ortiz <158114757+ortzfrancisco@users.noreply.github.com>
2024-04-07 12:40:51 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,78 +1,17 @@
 // TODO: Include packages needed for this application
-// npm install inquirer
-
+const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a description of your project:',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Provide installation instructions:',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Provide usage information:',
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'Provide contribution guidelines:',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Provide test instructions:',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Choose a license for your application:',
-        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub Username:',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your Email:',
-    },
+    ["input", "title", "What is the project title?: "],
+    ["input", "description", "What is the project description?: "],
+    ["input", "installation", "What is the project's installation instructions?: "],
+    ["input", "usage", "What is the usage information?: "],
+    ["input", "contributors", "Who else contributed to the project?: "],
+    ["input", "test", "What do you need to input to run tests?: "],
+    ["input", "github", "Please enter your GitHub username: "],
+    ["input", "email", "Please enter your email address: "],
+    ["list", "license", "What license are you using?: "],
 ];
 
-// TODO: Create a function to write README file
-const fs = require('fs');
-
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log('Successfully wrote to ' + fileName);
-    });
-}
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then((answers) => {
-        // Convert answers to markdown text
-        const markdown = generateMarkdown(answers);
-
-        // Write markdown to README.md
-        writeToFile('README.md', markdown);
-    });
-}
-
-// Function call to initialize app
- init();
